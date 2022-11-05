d5964c739d723aae79f90c4cda8532db235b1429
vhuhwi <66223771+vhuhwi@users.noreply.github.com>
2022-11-05 16:56:15 +0200

    Add files via upload

---
+++ start/simple_packing_tutorial_ep3/racecar.gd
@@ -0,0 +1,30 @@
+extends VehicleBody
+
+
+
+const STEER_SPEED = 1.5
+const STEER_LIMIT = .8
+var steer_target = 0
+
+export var engine_force_value = 100
+
+func _physics_process(delta):
+	_controler_logic(delta)
+
+func _controler_logic(delta):
+	
+	steer_target = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	steer_target *= STEER_LIMIT
+
+	if Input.is_action_pressed("ui_up"):
+		engine_force =engine_force_value
+	else:
+		engine_force = 0
+
+	if Input.is_action_pressed("ui_down"):
+		engine_force = -engine_force_value
+	else:
+		brake= 0
+
+	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
+	
